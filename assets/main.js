bf1eb60ee52cb7d9f96c1aa47e2de958955cf9c7
goelv641 <112566005+goelv641@users.noreply.github.com>
2023-04-04 15:05:14 -0400

    Homepage Toggle

---
+++ assets/main.js
@@ -0,0 +1,27 @@
+let yoga=document.querySelector("#yoga");
+let sound=document.querySelector("#sound");
+let yoga1=document.querySelector("#yoga1");
+let sound1=document.querySelector("#sound1");
+yoga1.style.display="none";
+sound1.style.display="none";
+yoga.addEventListener('click', function(){
+    console.log("click")
+    if(yoga1.style.display=="block"){
+        yoga1.style.display="none"
+    }
+    else{
+        yoga1.style.display="block"; 
+    }
+    sound1.style.display="none";
+})
+
+sound.addEventListener('click', function(){
+    console.log("click")
+    if(sound1.style.display=="block"){
+        sound1.style.display="none"
+    }
+    else{
+        sound1.style.display="block"; 
+    }
+    yoga1.style.display="none";
+})
